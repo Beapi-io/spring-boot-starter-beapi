eea15594aada71c905f0db0d7f1467e8bbff530d
Owen Rubel <orubel@gmail.com>
2023-02-27 10:04:11 -0800

    test

--- src/main/groovy/io/beapi/api/service/CliService.groovy
+++ src/main/groovy/io/beapi/api/service/CliService.groovy
@@ -42,11 +42,65 @@ public class CliService {
 
 
 	static transactional = false
+	
+	//void parse() {
+	//	println(argsList)
+	//}
 
+	void parse() {
+		ArrayList args = argsList.split(' ')
+		args.remove(0)
+		ArrayList validArgKeys = ['controller','connector','domain']
+		ArrayList scaffoldKeys = ['controller','connector']
+		ArrayList domainKey = ['domain']
+		LinkedHashMap vars = [:]
+		args.each(){
+			ArrayList temp = it.split('=')
+			if(validArgKeys.contains(temp[0].toLowerCase())){
+				if(temp[1] ==~ /[a-z][a-z0-9_]*(\.[a-z0-9_]+)+[0-9a-z_]/) {
+					switch(temp[0].toLowerCase()){
+						case 'controller':
+							if(controllerArg!=null){
+								error(1, "'controller' value has already been set. Please try again.")
+							}else{
+								controllerArg = temp[1]
+							}
+							break
+						case 'connector':
+							if(connectorArg!=null){
+								error(1, "'connector' value has already been set. Please try again.")
+							}else{
+								connectorArg = temp[1]
+							}
+							break
+						case 'domain':
+							if(domainArg!=null){
+								error(1, "'domain' value has already been set. Please try again.")
+							}else{
+								domainArg = temp[1]
+							}
+							break
+						default:
+							error(1, "Unrecognized arg. Please try again.")
+					}
+				}else{
+					error(1, "Invalid package name. Package name for '"+temp[0]+"' is not recognized as a valid package name")
+				}
+			}else{
+				error(1, "Invalid ARG sent. Please provide ARG values of 'controller/connector' and 'domain'.")
+			}
+		}
 
-	void test() {
-		println(argsList)
+		if(domainArg==null){
+			error(1, "Missing valid domain value sent. Please try again.")
+		}
 
+		if(controllerArg==null && connectorArg==null){
+			error(1, "Missing valid scaffold value sent (ie controller/connector). Please try again.")
+		}
+		println("domain : "+domainArg)
+		println("controller : "+controllerArg)
+		println("connector : "+connectorArg)
 	}
 
 }
