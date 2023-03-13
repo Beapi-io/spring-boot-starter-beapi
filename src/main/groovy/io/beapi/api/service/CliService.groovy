f75a3a282c1b2a8783738f04374bce0735e2023d
Owen Rubel <orubel@gmail.com>
2023-03-13 13:23:24 -0700

    cliservice for scaffolding connector nearly 100%; one final change needed. saving before completing

--- src/main/groovy/io/beapi/api/service/CliService.groovy
+++ src/main/groovy/io/beapi/api/service/CliService.groovy
@@ -29,7 +29,10 @@ import java.util.jar.JarFile
 import java.util.regex.Matcher
 import java.util.regex.Pattern
 import java.io.File
+
+import groovy.text.Template
 import groovy.text.GStringTemplateEngine
+
 import org.springframework.context.ApplicationContext
 import org.springframework.beans.factory.annotation.Value;
 import javax.persistence.EntityManager
@@ -37,7 +40,8 @@ import javax.persistence.metamodel.EntityType
 import javax.persistence.metamodel.Attribute
 import org.springframework.beans.factory.ListableBeanFactory
 import org.springframework.stereotype.Controller
-import java.lang.reflect.Field;
+import java.lang.reflect.Field
+import java.lang.reflect.Method
 import java.lang.annotation.Annotation;
 
 // todo: rename as ExchangeService : encompasses both request/response methods for interceptor
@@ -74,8 +78,8 @@ public class CliService {
 	private LinkedHashMap data = [:]
 	private Object obj;
 	String dirPath
-
-	//Integer cores = Holders.grailsApplication.config.apitoolkit.procCores as Integer
+	String templateDir = "${System.getProperty('user.dir')}/src/main/groovy/templates/"
+	List variables = []
 
 	public CliService(ApplicationContext applicationContext) {
 		this.ctx = applicationContext
@@ -140,36 +144,22 @@ public class CliService {
 				if (domainArg) {
 					def entityManager = ctx.getBean('entityManagerFactory')
 					Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+					LinkedHashMap values = [:]
 					for (EntityType tempEntityType : entities) {
 						if (!domainFound) {
 							if (tempEntityType.getJavaType().getCanonicalName() == domainArg) {
 
-//println("domain : " + tempEntityType.getJavaType())
-//println("domain : " + tempEntityType.getJavaType().getCanonicalName())
-//println("domain : " + tempEntityType.getJavaType().getName())
-//println("domain : " + tempEntityType.getJavaType().getPackage().getName())
-//println("domain : " + tempEntityType.getDeclaredAttributes())
-
 								domainFound = true
-
-								//entityType.getSimpleName().toLowerCase().concat("s");
 
 								// todo : should this be camelCase????
 								realPackageName = tempEntityType.getJavaType().getCanonicalName() - (tempEntityType.getJavaType().getPackage().getName() + ".")
 								realName = realPackageName.toLowerCase()
-//println(realPackageName)
-//println(realName)
-
-								data[realName] = [:]
-								data[realName]['className'] = realPackageName
-								data[realName]['values'] = [:]
 
 								Field[] fields = tempEntityType.getJavaType().getDeclaredFields()
 								fields.each() {
 
 									String attName = it.getName()
-									println("${it.getType().getCanonicalName()} / ${attName}")
-
 									Annotation anno = it.getAnnotation(javax.persistence.Column.class);
 									LinkedHashMap constraints = [:]
 
@@ -185,27 +175,45 @@ public class CliService {
 										}
 									}
 
-									if (anno != null) {
-										constraints['nullable'] = anno.nullable()
-										constraints['unique'] = anno.unique()
-										if (keyType) {
-											if (reference) {
-												//FKEY
-												data[realName]['values'][it.getType().getName()] = ['key': keyType, 'reference': reference, 'type': it.getType().getCanonicalName(), 'constraints': constraints, 'description': '<put your description here>', 'mockData': '<put your mock data here>']
+									if(attName != 'serialVersionUID') {
+										variables.add("${attName}")
+										if (anno != null) {
+
+											constraints['nullable'] = anno.nullable()
+											constraints['unique'] = anno.unique()
+											if (keyType) {
+
+												if (reference) {
+													//FKEY
+													values[attName] = ['key': keyType, 'reference': reference, 'type': it.getType().getCanonicalName(), 'constraints': constraints, 'description': '<put your description here>', 'mockData': '<put your mock data here>']
+												} else {
+													//PKEY
+													values[attName] = ['key': keyType, 'type': it.getType().getCanonicalName(), 'constraints': constraints, 'description': '<put your description here>', 'mockData': '<put your mock data here>']
+												}
 											} else {
-												//PKEY
-												data[realName]['values'][it.getType().getName()] = ['key': keyType, 'type': it.getType().getCanonicalName(), 'constraints': constraints, 'description': '<put your description here>', 'mockData': '<put your mock data here>']
+												values[attName] = ['type': it.getType().getCanonicalName(), 'constraints': constraints, 'description': '<put your description here>', 'mockData': '<put your mock data here>']
 											}
 										} else {
-											data[realName]['values'][it.getType().getName()] = ['type': it.getType().getCanonicalName(), 'constraints': constraints, 'description': '<put your description here>', 'mockData': '<put your mock data here>']
+											values[attName] = ['type': it.getType().getCanonicalName(), 'constraints': null, 'description': '<put your description here>', 'mockData': '<put your mock data here>']
 										}
-									} else {
-										data[realName]['values'][it.getType().getName()] = ['type': it.getType().getCanonicalName(), 'constraints': null, 'description': '<put your description here>', 'mockData': '<put your mock data here>']
 									}
 								}
 							}
 						}
 					}
+
+					// create attList
+					data['realName'] = realName
+					if(connectorArg) {
+						println(values)
+						data['attList'] = createAttList(values)
+					}
+
+					// todo : create method to check if controller exists
+					// if exists, fill out 'URI' json
+					// else template URI json using params from values
+					String uris = createUriAtts(realName)
+					data['uris'] = uris
 
 					if (domainFound) {
 						if (controllerArg) {
@@ -219,6 +227,164 @@ public class CliService {
 				}
 			}
 		}
+	}
+
+	private String createUriAtts(String controllerName){
+		ArrayList ignoreList = [
+				'setMetaClass',
+				'getMetaClass',
+				'handleRequest',
+				'formatMap',
+				'formatEntity',
+				'convertModel',
+				'parseResponseParams',
+				'writeErrorResponse',
+				'invokeMethod',
+				'getProperty',
+				'setProperty',
+				'wait',
+				'notify',
+				'notifyAll',
+				'getClass',
+				'hashCode',
+				'toString',
+				'equals','wait',
+				'getTraceService',
+				'setTraceService'
+		]
+
+		// todo: create json String from 'variables' arraylist for use below
+		String varString = createVarString(variables)
+
+		String uris = ""
+		LinkedHashMap<String, Object> cont = listableBeanFactory.getBeansWithAnnotation(org.springframework.stereotype.Controller.class)
+		cont.each() { k, v ->
+			if(k == controllerName){
+				println(v.getClass())
+
+				Method[] actions = v.getClass().getMethods()
+				// get methods as 'actions'
+				actions.each() { it4 ->
+					if(!ignoreList.contains(it4.getName())) {
+						String method = ""
+						String req
+						String resp
+						Pattern listPattern = Pattern.compile("list|listBy")
+						Pattern getPattern = Pattern.compile("get|getBy|show|showBy|enable")
+						Pattern postPattern = Pattern.compile("create|make|generate|build|save|new")
+						Pattern putPattern = Pattern.compile("edit|update")
+						Pattern deletePattern = Pattern.compile("delete|deleteBy|disable|disableBy|destroy|kill|reset|resetBy")
+
+						Matcher getm = getPattern.matcher(it4.getName())
+						if (getm.find()) {
+							method = 'GET'
+							req = "[\"id\"]"
+							resp = varString
+						}
+
+						Matcher listm = listPattern.matcher(it4.getName())
+						if (listm.find()) {
+							method = 'GET'
+							resp = varString
+						}
+
+						if (method.isEmpty()) {
+							Matcher postm = postPattern.matcher(it4.getName())
+							if (postm.find()) {
+								method = 'POST'
+								req = varString
+								resp = "[\"id\"]"
+							}
+						}
+
+						if (method.isEmpty()) {
+							Matcher putm = putPattern.matcher(it4.getName())
+							if (putm.find()) {
+								method = 'PUT'
+								req = varString
+								resp = "[\"id\"]"
+							}
+						}
+
+						if (method.isEmpty()) {
+							Matcher delm = deletePattern.matcher(it4.getName());
+							if (delm.find()) {
+								method = 'DELETE'
+								req = "[\"id\"]"
+								resp = "[\"id\"]"
+							}
+						}
+
+
+String uri = """
+\t\t\t\t\"${it4.getName()}\": {
+\t\t\t\t\t\"METHOD\": "${method}",
+\t\t\t\t\t\"DESCRIPTION\": \"Description for ${it4.getName()}\",
+\t\t\t\t\t"ROLES\": {
+\t\t\t\t\t\t"BATCH\": [\"ROLE_ADMIN\"]
+\t\t\t\t\t},
+\t\t\t\t\t\"REQUEST\": {
+\t\t\t\t\t\t\"permitAll\": ${req}
+\t\t\t\t\t},
+\t\t\t\t\t\"RESPONSE\": {
+\t\t\t\t\t\t\"permitAll\": ${resp}
+\t\t\t\t\t}
+\t\t\t\t},"""
+						uris <<= uri
+					}
+				}
+			}
+
+		}
+		return uris
+	}
+
+	private String createVarString(ArrayList variables){
+		String varString = "["
+		int inc = 1
+		variables.each(){
+			varString += "\"${it}\""
+			if(inc!=variables.size()){
+				varString += ","
+				inc++
+			}
+		}
+		varString += "]"
+		return varString
+	}
+
+	private String createAttList(LinkedHashMap values){
+		String json = ""
+		int inc=1
+		values.each() { k, v ->
+json += """
+\t\t"${k}": {"""
+			if (v.keyType == 'PKEY') {
+				json += "\t\t\t\"key\": \"${v.keyType}\","
+			} else if (v.keyType == 'FKEY') {
+json += """\t\t\t\t"key": "${v.keyType}",
+\t\t\t\t"references": "","""
+			}
+
+json += """
+\t\t\t"type": "${entityTypeConverter(v.type)}",
+\t\t\t"description": \"\",
+\t\t\t"mockData": \"\","""
+
+			if(v.constraints){
+json += """
+\t\t\t\"constraints\": {\"order\":${inc},\"isNullable\":${v.constraints.nullable}, \"isUnique\":${v.constraints.unique}},"""
+			}else{
+json += """
+\t\t\t\"constraints\": {\"order\":${inc}},"""
+			}
+
+json += """
+\t\t},"""
+			inc++
+		}
+
+		return json
 	}
 
 	private void createController(LinkedHashMap data){
@@ -238,8 +404,10 @@ public class CliService {
 
 		// check directory structure (in case this is FIRST controller)
 		String controllerPath = controllerArg.replaceAll("\\.","/");
-		String groovyPath = System.getProperty("user.dir")+"/src/main/groovy/${path}"
-		if(!controllerFound && !dirExists(groovyPath)){
+		String pathType
+		if(dirExists("${System.getProperty('user.dir')}/src/main/groovy/${path}")){ pathType='groovy' }
+		if(dirExists("${System.getProperty('user.dir')}/src/main/java/${path}")){ pathType='java' }
+		if(!controllerFound && !pathType==null){
 			error(1, "Sent controller class did not match any existing package using the 'Controller' annotation NOR directory structure. Please try again with the full package.")
 		}
 
@@ -252,36 +420,24 @@ public class CliService {
 		println("### creating connector...")
 		println(System.getProperty('user.home'))
 
-		String connectorPath = System.getProperty('user.home')+"${connectorDir}"
-		println(connectorPath)
+		String connectorPath = "${System.getProperty('user.home')}/${connectorDir}"
 		if(!dirExists(connectorPath)){
-			println("need to create path...")
-			//error(1, "Sent controller class did not match any existing package using the 'Controller' annotation NOR directory structure. Please try again with the full package.")
+			// need to create path
+			error(1, "The 'iostateDir' in your 'beapi_api.yml' file is not porperly defined as the directory does not exist. Please check and try again.")
 		}
 
-		println(data)
+
+		writeConnector("templates/Connector.json.template", "${System.getProperty('user.home')}/${connectorDir}/${realPackageName}.json", data)
+
 		error(0, "")
 	}
 
 	private boolean dirExists(String path) {
 		boolean exists = false
-
-		// src/main/groovy
-
 		//def ant = new AntBuilder()
-		def gfile = new File(groovyPath)
-		if (gfile.exists()) {
+		def file = new File(path)
+		if (file.exists()) {
 			exists = true
-			dirPath = groovyPath
-			return exists
-		}
-
-		// src/main/java
-		String javaPath = System.getProperty("user.dir")+"/src/main/java/${path}"
-		def jfile = new File(javaPath)
-		if (jfile.exists()) {
-			exists = true
-			dirPath = javaPath
 			return exists
 		}
 		return exists
@@ -303,74 +459,144 @@ public class CliService {
 	}
 
 
-	LinkedHashMap entityTypeConverter(String type){
+	void writeConnector(String inPath, String outPath, LinkedHashMap attribs){
+		String starterDir = new File(getClass().protectionDomain.codeSource.location.path).path
+		def starter = new File(starterDir)
+		if (starter.isFile() && starter.name.endsWith("jar")) {
+			JarFile jar = new JarFile(starter)
+			JarEntry entry = jar.getEntry(inPath)
+
+			InputStream inStream = jar.getInputStream(entry)
+
+			OutputStream out = new FileOutputStream(outPath)
+			int c
+			while ((c = inStream.read()) != -1) {
+				out.write(c)
+			}
+			inStream.close()
+			out.close()
+			jar.close()
+
+			def templateFile = new File(outPath)
+			def engine = new groovy.text.GStringTemplateEngine()
+			def template = engine.createTemplate(templateFile).make(attribs)
+
+			String controller = template.toString()
+
+			BufferedWriter writer = new BufferedWriter(new FileWriter(outPath))
+			writer.write(controller)
+			writer.close()
+		}else{
+			// throw error
+			error(1, "Project is trying to use CLI but does not use 'spring-boot-starter-beapi'. Please include the starter to use this service.");
+		}
+	}
+
+
+
+	/*
+	void writeController(String inPath, String outPath, LinkedHashMap attribs){
+		String pluginDir = new File(getClass().protectionDomain.codeSource.location.path).path
+		def plugin = new File(pluginDir)
+		try {
+			if (plugin.isFile() && plugin.name.endsWith("jar")) {
+				JarFile jar = new JarFile(plugin)
+				JarEntry entry = jar.getEntry(inPath)
+				InputStream inStream = jar.getInputStream(entry)
+				OutputStream out = new FileOutputStream(outPath)
+				int c
+				while ((c = inStream.read()) != -1) {
+					out.write(c)
+				}
+				inStream.close()
+				out.close()
+				jar.close()
+
+				def templateFile = new File(outPath)
+				def engine = new groovy.text.GStringTemplateEngine()
+				def template = engine.createTemplate(templateFile).make(attribs)
+
+				String controller = template.toString()
+
+				BufferedWriter writer = new BufferedWriter(new FileWriter(outPath))
+				writer.write(controller)
+				writer.close()
+			}
+		}catch(Exception e){
+			println("Exception. Please submit error report :"+e)
+		}
+	}
+
+	 */
+
+
+
+
+	String entityTypeConverter(String type){
 		switch(type){
 			case 'char':
 			case 'java.lang.Character':
-				return ['Character': type]
+				return 'Character'
 				break
 			case 'bool':
 			case 'java.lang.Boolean':
-				return ['Boolean':'java.lang.Boolean']
+				return 'Boolean'
 				break
 			case 'byte':
 			case 'java.lang.Byte':
-				return ['Byte':'java.lang.Byte']
+				return 'Byte'
 				break
 			case 'short':
 			case 'java.lang.Short':
-				return ['Short':'java.lang.Short']
+				return 'Short'
 				break
 			case 'int':
 			case 'java.lang.Integer':
-				return ['Integer':'java.lang.Integer']
+				return 'Integer'
 				break
 			case 'long':
 			case 'java.lang.Long':
-				return ['Long':'java.lang.Long']
+				return 'Long'
 				break
 			case 'float':
 			case 'java.lang.Float':
-				return ['Float':'java.lang.Float']
+				return 'Float'
 				break
 			case 'double':
 			case 'java.lang.Double':
-				return ['Double':'java.lang.Double']
+				return 'Double'
 				break
 			case 'java.math.BigInteger':
-				return ['BigInteger':'java.math.BigInteger']
+				return 'BigInteger'
 				break
 			case 'java.math.BigDecimal':
-				return ['BigDecimal':'java.math.BigDecimal']
+				return 'BigDecimal'
 				break
 			case 'java.sql.Timestamp':
-				return ['Timestamp':'java.sql.Timestamp']
+				return 'Timestamp'
 				break
 			case 'java.sql.Date':
 			case 'java.util.Date':
-				return ['Date':'java.util.Date']
-				break
-			case 'java.util.Calendar':
-				return ['Calendar':'java.util.Calendar']
+				return 'Date'
 				break
 			case 'java.util.Currency':
-				return ['Currency':'java.util.Currency']
+				return 'Currency'
 				break
 			case 'java.util.Locale':
-				return ['Locale':'java.util.Locale']
+				return 'Locale'
 				break
 			case 'java.util.TimeZone':
-				return ['TimeZone':'java.util.TimeZone']
+				return 'TimeZone'
 				break
 			case 'java.net.URL':
-				return ['URL':'java.net.URL']
+				return 'URL'
 				break
 			case 'java.util.UUID':
-				return ['UUID':'java.util.UUID']
+				return 'UUID'
 				break
 			case 'java.lang.String':
 			default:
-				return ['String':'java.lang.String']
+				return 'String'
 				break
 		}
 	}
